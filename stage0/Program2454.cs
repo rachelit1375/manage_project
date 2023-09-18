5077d4cf92b7e92adf8249770f9af2b35499c997
rachelit1375 <rachelit1375@gmail.com>
2023-09-18 17:13:45 +0300

    initial code  + Welcome functions

---
+++ stage0/Program2454.cs
@@ -0,0 +1,19 @@
+﻿namespace stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2454();
+            Welcome3323();
+            Console.ReadKey();
+        }
+        static partial void Welcome3323();
+        private static void Welcome2454()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
