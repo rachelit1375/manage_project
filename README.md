0eb334ac8c03a7005a231c709f0a7ca6c372c318
OsnatShachor <osnaty16@gmail.com>
2023-10-16 01:09:09 +0300

    I update what I have to do and welcomw function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dotNet5784_3323_2454
+hi, is it work?
+
---
+++ stage0/Program3323.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+
+namespace stage0
+{
+    partial class Program
+    {
+        static partial void Welcome3323()
+        {
+            Console.WriteLine("I am also here!");
+
+        }
+    }
+}
