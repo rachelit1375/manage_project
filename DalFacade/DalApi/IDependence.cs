8c73e928d9c480ba5479a2a57d32039ee2ad336d
OsnatShachor <osnaty16@gmail.com>
2023-10-31 17:04:37 +0200

    We are done entities

---
+++ DalFacade/DalApi/IDependence.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DalApi;
+using DO;
+public interface IDependence
+{
+    int Create(Dependence item); //Creates new entity object in DAL
+    Dependence? Read(int id); //Reads entity object by its ID 
+    List<Dependence> ReadAll(); //stage 1 only, Reads all entity objects
+    void Update(Dependence item); //Updates entity object
+    void Delete(int id); //Deletes an object by its Id
+}
