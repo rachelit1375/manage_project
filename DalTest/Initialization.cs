55520c54d2a26a5dd9e60f68b4a0245d8707a44e
rachelit1375 <rachelit1375@gmail.com>
2023-11-07 14:59:34 +0200

    initialization

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -2,69 +2,77 @@
 namespace DalTest;
 using DalApi;
 using DO;
+using System.Reflection.Emit;
+
 /// <summary>
 /// 
 /// </summary>
 public static class Initialization
 {
-    private static ITask? s_dalTask;//מה זה אומר? איזה סוג אובייקט זה?
+    private static ITask? s_dalTask;
     private static IDependence? s_dalDependence;
     private static IEngineer? s_dalEngineer;
     private static readonly Random s_rand = new();
-
-    private static void createTasks()
-    {
-        string[] tasksAliases =
-        {
-            "wash the dishes",
-            "put the dishes in their place",
-            "do homework",
-            "tide the bed",
-            "go to shoping",
-            "buy milk"
-        };
-
-        foreach (var taskAlias in tasksAliases)//לוודא שלא בעיה לקרוא לרבים עם es
-        {
-            int id;
-            do
-            {
-                id = s_rand.Next(1000, 9999);
-            }
-            while (s_dalTask!.Read(id) != null);
-            int year=s_rand.Next(2020,2023);
-            int month=s_rand.Next(1,12);
-            int day=s_rand.Next(1,30);
-            DateTime createDate= new DateTime(year, month, day);
-            int dMonth=month+1;
-            int dYear=year;
-            if (month==12)
-            {
-                dMonth = 1;
-                dYear=year+1;
-            }         
-            DateTime deadLineDate = new DateTime(dYear, dMonth, day);
-            Task newTask=new(id,null, taskAlias,false, createDate,null, null, null, deadLineDate,null,null,null...);
-            s_dalTask!.Create(newTask);
-
-        }
-    }
-    private static void createDependences()
-    {
-
-    }
     private static void createEngineers()
     {
         (string name, string email)[] engineerNames =
         {
-           ( "Racheli Toledano", "racheli@gmail.com"),
-           ( "Osnat Shachor", "osnaty@gmail.com"),
-           ( "Dani Levi", "Dani@gmail.com"),
-           ( "Eli Amar", "Eli@gmail.com"),
+           ("Racheli Toledano", "racheli@gmail.com"),
+           ("Osnat Shachor", "osnaty@gmail.com"),
+           ("Dani Levi", "Dani@gmail.com"),
+           ("Eli Amar", "Eli@gmail.com"),
            ("Yair Cohen","Yair@gmail.com"),
            ("Ariela Levin","Ariel@gmail.com"),
            ("Dina Klein", "Dina@gmail.com"),
            ("Zundel Baruch", "Zundel@gmail.com")
         };
+        EngineerExperience level;
+        int id;
+        foreach (var engineerName in engineerNames)
+        {
+            do
+                id = s_rand.Next(200000000, 400000000);
+            while (s_dalEngineer!.Read(id) != null);
+            level = (EngineerExperience)s_rand.Next(0, 3);
+            Engineer engineer = new(id, engineerName.name, engineerName.email, level, null);
+        }
     }
+    private static void createTasks()
+    {
+        (string taskAlias, string description)[] tasks =
+          {
+           ("Racheli Toledano", "racheli@gmail.com"),
+           ("Osnat Shachor", "osnaty@gmail.com"),
+           ("Dani Levi", "Dani@gmail.com"),
+           ("Eli Amar", "Eli@gmail.com"),
+           ("Yair Cohen","Yair@gmail.com"),
+           ("Ariela Levin","Ariel@gmail.com"),
+           ("Dina Klein", "Dina@gmail.com"),
+           ("Zundel Baruch", "Zundel@gmail.com")
+        };
+
+        EngineerExperience level;
+        int id;
+
+        foreach (var task in tasks)//לוודא שלא בעיה לקרוא לרבים עם es
+        {
+            do
+            {
+                id = s_rand.Next(1000, 10000);
+            }
+            while (s_dalTask!.Read(id) != null);
+
+            DateTime createDate = DateTime.Now;
+            DateTime startDate = createDate.AddDays(s_rand.Next(0, 11));
+            DateTime ScheduledDate = createDate.AddDays(s_rand.Next(11, 61));
+            DateTime ForecastDate = createDate.AddDays(s_rand.Next(11, 61));
+            DateTime deadLineDate = ForecastDate.AddDays(s_rand.Next(0, 14));
+            level = (EngineerExperience)s_rand.Next(0, 3);
+            Task newTask = new(id, task.description, task.taskAlias, false, createDate, startDate, ScheduledDate, ForecastDate, deadLineDate, null, null, null, level);
+            s_dalTask!.Create(newTask);
+
+        }
+    }
+
+
 }
